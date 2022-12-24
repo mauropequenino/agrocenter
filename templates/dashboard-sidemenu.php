0c23a806b9de314535761cbef65f979a43594b43
Mauro Pequenino <mauroernesto5@gmail.com>
2022-12-24 14:54:23 +0200

    Update dashboard-sidemenu.php

--- templates/dashboard-sidemenu.php
+++ templates/dashboard-sidemenu.php
@@ -29,7 +29,7 @@
     <title>Agrocenter</title>
      <!-- CSS only -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+     <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
      <!----font awesome---->
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
 </head>
@@ -38,18 +38,18 @@
         <div class="row flex-nowrap">
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-success">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="index.html" class="navbar-brand float-end logo logo-dash">
+                    <a href="<?= $BASE_URL ?>index.php" class="navbar-brand float-end logo logo-dash">
                         <i class="fas fa-leaf"></i>
                         <!-- <span>AgroCenter</span> -->
                     </a>
                     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="sidebar-menu">
                         <li class="nav-item">
-                            <a href="<?= $BASE_URL ?>/index.php" class="nav-link align-middle px-0">
+                            <a href="<?= $BASE_URL ?>index.php" class="nav-link align-middle px-0">
                               <i class="fas fa-home"></i><span class="ms-1 d-none d-sm-inline">Página inicial</span>
                             </a>
                         </li>
                         <li>
-                            <a href="<?= $BASE_URL ?>/dashboard.php" class="nav-link px-0 align-middle">
+                            <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link px-0 align-middle">
                               <i class="fas fa-table"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
                             </a>
                         </li>
@@ -61,8 +61,8 @@
                             <span class="d-none d-sm-inline mx-1"><?= $userData->name ?></span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                            <li><a class="dropdown-item" href="<?= $BASE_URL ?>/editprofile.php">Perfil</a></li>
-                            <li><a class="dropdown-item" href="<?= $BASE_URL ?>/changepassword.php">Alterar senha</a></li>
+                            <li><a class="dropdown-item" href="<?= $BASE_URL ?>editprofile.php">Perfil</a></li>
+                            <li><a class="dropdown-item" href="<?= $BASE_URL ?>changepassword.php">Alterar senha</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
